56ad847b6df23109a66536982f3ea7d3795d2390
HieuPhan33 <hieup007@gmail.com>
2022-03-15 23:00:32 +0000

    First commit - CS no work

---
+++ scripts/voc/rkd_scl_15-5.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+start=`date +%s`
+
+START_DATE=$(date '+%Y-%m-%d')
+
+PORT=$((9000 + RANDOM % 1000))
+GPU=2,3
+NB_GPU=3
+
+
+DATA_ROOT=/media/Z/data/PascalVOC12
+
+DATASET=voc
+TASK=15-5
+LOSS=0.7
+NAME=RKD_SCL
+METHOD=PLOP
+
+OPTIONS="--checkpoint checkpoints/step/"
+
+SCREENNAME="${DATASET}_${TASK}_${NAME} On GPUs ${GPU}"
+
+RESULTSFILE=results/${START_DATE}_${DATASET}_${TASK}_${NAME}.csv
+rm -f ${RESULTSFILE}
+
+echo -ne "\ek${SCREENNAME}\e\\"
+
+echo "Writing in ${RESULTSFILE}"
+
+# If you already trained the model for the first step, you can re-use those weights
+# in order to skip this initial step --> faster iteration on your model
+# Set this variable with the weights path
+# FIRSTMODEL=/path/to/my/first/weights
+# Then, for the first step, append those options:
+# --ckpt ${FIRSTMODEL} --test
+# And for the second step, this option:
+# --step_ckpt ${FIRSTMODEL}
+
+BATCH_SIZE=12
+INITIAL_EPOCHS=30
+EPOCHS=30
+FIRSTMODEL=checkpoints/step/15-5s-voc_RKD_0.pth
+
+#CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --ckpt ${FIRSTMODEL} --scl 0.25 --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 0 --lr 0.01 --epochs ${INITIAL_EPOCHS} --method ${METHOD} --rebal_kd ${LOSS} --opt_level O1 ${OPTIONS}
+CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --scl 0.05 --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 1 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --rebal_kd ${LOSS} --opt_level O1 ${OPTIONS}
+python3 average_csv.py ${RESULTSFILE}
+
+echo ${SCREENNAME}
+
+
+end=`date +%s`
+runtime=$((end-start))
+echo "Run in ${runtime}s"
